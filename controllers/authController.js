97b86c841ac192a3361d26d8cef5a1a95a86319d
Sagar <ishowsagar777@gmail.com>
2025-12-11 11:01:57 +0530

    working on session and security

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 import validator from "validator";
+import bcrypt from "bcryptjs";
+import { getDBConnection } from "../db/db.js";
 export async function registerUser(req, res) {
   // console.log('req body :',req.body)
 
@@ -27,6 +29,55 @@ export async function registerUser(req, res) {
   //   validate email formats
   if (!validator.isEmail(email)) {
     return res.status(400).json({ error: "email format is wrong" });
+  }
+
+  // * Adding new users to our db table - users
+  try {
+    const db = await getDBConnection();
+    const params = [email, username];
+
+    // select that entry from table where email n username matches provided email & username
+    const userExists = await db.get(
+      `
+     select id from users 
+     where email = ? or username = ?
+     `,
+      params
+    );
+
+    // if user exists in db
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ error: "Email or username already in use." });
+    }
+
+    //* handeling security concerns with bcrypt
+
+    // hashing pass --> more secured random pass
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    // if user doesn't exists in db --> insert POSt data-entry to users table
+    const placeholderActualValues = [name, email, username, hashedPass];
+    const userEntryAddedToDB = await db.run(
+      `
+      insert into users (name,email,username,password)
+        values (?,?,?,?)
+      `,
+      placeholderActualValues
+    );
+
+    // ! Extracting and storing last user entry to session
+    // fetching last user id that added to db
+    const { lastID } = userEntryAddedToDB;
+
+    // --> storing that id in session
+    req.session.userId = lastID;
+
+    res.status(201).json({ message: "User registered" });
+  } catch (err) {
+    console.error("Registration error:", err.message);
+    res.status(500).json({ error: "Registration failed. Please try again." });
   }
 }
 
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { productsRouter } from "./routes/product.js";
 import { authRouter } from "./routes/auth.js";
+import session from "express-session";
+
 const app = express();
 // express code is executed from top to bottom 👇
 // next --> component of middleware --> gets called when fn of middlewar (app.use) is completed
@@ -20,6 +22,30 @@ app.use("/api/products", productsRouter);
 // * collects all chunks --> turn into body --> pass body to request object
 app.use(express.json()); //--> if successfully parsed, calls next() --> invokes the next mw fnc
 
+// !handles session created by --> client
+app.use(
+  session({
+    // could be str or array of str which --> validates session from cookies
+    secret: "we will later store into env variable",
+
+    // makes sures it does not re-save every session from client
+    resave: false,
+
+    // prevents empty sessions from being stored when client even won't login
+    saveUninitialized: false,
+
+    // cookie
+    cookie: {
+      // read only in general --> no injection
+      httpOnly: true,
+
+      // set false as --> we're on http, not on https
+      secure: false,
+
+      sameSite: "lax",
+    },
+  })
+);
 // handles sign-in/up routes from authRouter
 app.use("/api/auth", authRouter);
 
